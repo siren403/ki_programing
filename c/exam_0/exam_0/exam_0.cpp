54dd1337c734286848ea6c410a8c2888f4d64ba4
qkrsogusl3 <qkrsogusl3@gmail.com>
2016-12-19 16:25:11 +0900

    programing basic 1

---
+++ c/exam_0/exam_0/exam_0.cpp
@@ -0,0 +1,77 @@
+// exam_0.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+
+	//printf("Hello World\n");
+	/*
+	int tA = 0;
+	int tB = 0;
+
+	tA = 3;
+	tB = 2;
+
+	float tResult = 0;
+
+	tResult = tA + tB;
+	printf("%f\n", tResult);
+
+
+	tResult = tA - tB;
+	printf("%f\n", tResult);
+
+
+	tResult = tA * tB;
+	printf("%f\n", tResult);
+
+
+	tResult = (float)tA / tB;
+	printf("%f\n", tResult);
+	*/
+
+	/*
+	int ti = 0;
+	int tj = 0;
+	int tResult = 0;
+
+	for (ti = 1; ti <= 9; ti++)
+	{
+		for (tj = 1; tj <= 9; tj++) 
+		{
+			tResult = ti * tj;
+
+			printf("%d * %d", ti, tj);
+			printf("= %d", tResult);
+			printf("\n");
+		
+		}
+	}
+	*/
+
+
+	int tInput = 0;
+
+	while (1)
+	{
+		tInput = scanf_s("%d", &tInput);
+	}
+
+	printf("==================\n");
+
+	//int tInput = 0;
+	//scanf_s("%d", &tInput);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
