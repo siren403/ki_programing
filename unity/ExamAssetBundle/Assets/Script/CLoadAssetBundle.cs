1c602b9b4763311ed772fc90239ad1f5475eb059
qkrsogusl3 <qkrsogusl3@gmail.com>
2017-04-10 17:31:52 +0900

    에셋 번들 웹 다운로드

--- unity/ExamAssetBundle/Assets/Script/CLoadAssetBundle.cs
+++ unity/ExamAssetBundle/Assets/Script/CLoadAssetBundle.cs
@@ -2,40 +2,188 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct GUIRect
+{
+    private Rect mRect;
+    public Rect rect
+    {
+        get
+        {
+            mRect.x = mCenter.x - mSize.x * 0.5f;
+            mRect.y = mCenter.y - mSize.y * 0.5f;
+
+            mRect.size = mSize;
+            return mRect;
+        }
+    }
+
+    private Vector2 mCenter;
+    private Vector2 mSize;
+
+    public Vector2 center
+    {
+        set
+        {
+            mCenter = value;
+        }
+    }
+    public Vector2 size
+    {
+        set
+        {
+            mSize = value;
+        }
+    }
+    
+    public GUIRect(float x, float y,float width,float height)
+    {
+        mCenter = new Vector2(x, y);
+        mSize = new Vector2(width, height);
+        mRect = Rect.zero;
+    }
+}
+
+
 public class CLoadAssetBundle : MonoBehaviour
 {
 
-    public GameObject Sample = null;
+    public enum LoadType { StreamingAssets, WebServer }
+
+    public LoadType mLoadType = LoadType.StreamingAssets;
+
+    private bool isLoading = false;
+
+    public string SampleURL = string.Empty;
 
     // Use this for initialization
-    IEnumerator Start()
+    void Start()
     {
+        
+    }
 
+    private void OnGUI()
+    {
+        float width = Screen.width;
+        float height = Screen.height;
+
+        GUIRect guiRect = new GUIRect();
+
+        guiRect.center = new Vector2(width * 0.5f, height * 0.5f);
+        guiRect.size = new Vector2(width * 0.65f, height * 0.1f);
+        if (GUI.Button(guiRect.rect, "Load All Asset Bundle"))
+        {
+            if(isLoading == false)
+            {
+                StartCoroutine(LoadAllAssetBundles());
+            }
+        }
+        guiRect.center = new Vector2(width * 0.5f, height * 0.6f);
+        if (GUI.Button(guiRect.rect, "Sample Download"))
+        {
+            StartCoroutine(LoadSampleAssetBundle());
+        }
+
+
+        guiRect.center = new Vector2(width * 0.5f, height * 0.9f);
+        if(GUI.Button(guiRect.rect,"Remove Cache"))
+        {
+            bool result = Caching.CleanCache();
+            Debug.Log("result : " + result);
+        }
+
+
+    }
+
+    public IEnumerator LoadAllAssetBundles()
+    {
+        isLoading = true;
+        string URL = string.Empty;
         string path = string.Empty;
 #if UNITY_EDITOR
-        path = "file:///" + Application.streamingAssetsPath + "/AssetBundles";
+
+        switch (mLoadType)
+        {
+            case LoadType.StreamingAssets:
+                URL = "file:///" + Application.streamingAssetsPath + "/";
+                break;
+            case LoadType.WebServer:
+                URL = "http://cjcj09.dothome.co.kr/AssetBundles/";
+                break;
+        }
+        path = URL + "AssetBundles";
+#elif UNITY_STANDALONE_WIN
+        path = "";
 #endif
         Debug.Log(path);
 
+        AssetBundleManifest assetBundleManifest = null;
         using (WWW www = new WWW(path))
         {
             yield return www;
 
             AssetBundle bundleManifest = www.assetBundle;
 
-            AssetBundleManifest assetBundleManifest = bundleManifest.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
-
-            foreach (var bundleName in assetBundleManifest.GetAllAssetBundles())
-                Debug.Log(bundleName);
+            assetBundleManifest = bundleManifest.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
 
         }
+
+        if (assetBundleManifest != null)
+        {
+            foreach (var assetBundleName in assetBundleManifest.GetAllAssetBundles())
+            {
+                Debug.Log("---------------------");
+
+                Debug.Log(assetBundleName);
+                bool isCaching = Caching.IsVersionCached(URL + assetBundleName, assetBundleManifest.GetAssetBundleHash(assetBundleName));
+                Debug.Log(string.Format("isCaching : {0}\nURL : {1}\nHash : {2}", isCaching, URL + assetBundleName, assetBundleManifest.GetAssetBundleHash(assetBundleName)));
+
+                using (WWW www = WWW.LoadFromCacheOrDownload(URL + assetBundleName, assetBundleManifest.GetAssetBundleHash(assetBundleName)))
+                {
+                    yield return www;
+
+
+                    AssetBundle bundle = www.assetBundle;
+
+                    foreach (var name in bundle.GetAllAssetNames())
+                    {
+                        Debug.Log(name);
+                        GameObject prefab = bundle.LoadAsset<GameObject>(name);
+                        if (prefab != null)
+                        {
+                            Instantiate(prefab);
+                        }
+                    }
+
+                    bundle.Unload(false);
+                }
+
+                Debug.Log("---------------------");
+
+            }
+        }
+        isLoading = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator LoadSampleAssetBundle()
     {
+        using (WWW www = WWW.LoadFromCacheOrDownload(SampleURL, 0))
+        {
+            yield return www;
 
+            AssetBundle assetBundle = www.assetBundle;
+
+            foreach (var name in assetBundle.GetAllAssetNames())
+            {
+                Debug.Log(name);
+                GameObject prefab = assetBundle.LoadAsset<GameObject>(name);
+                if (prefab != null)
+                {
+                    Instantiate(prefab);
+                }
+            }
+
+            assetBundle.Unload(false);
+        }
     }
-
 
 }
---
+++ unity/ExamAssetBundle/readme.md
@@ -0,0 +1,42 @@
+### Using Asset
+Level 1 Monster Pack
+
+---
+
+Resources : Nothing
+StreamingAssets : Nothing
+InspectorLink : Nothing
+
+Size : 10.2MB
+
+---
+
+Resources : Nothing
+StreamingAssets : Nothing
+InspectorLink : Bat_Green
+
+Size : 15.5MB
+
+---
+
+Resources : Bat_Green
+StreamingAssets : Nothing
+InspectorLink : Nothing
+
+Size : 15.6MB
+
+---
+
+Resources : Nothing
+StreamingAssets : Bat_Green(AssetBundle)
+InspectorLink : Nothing
+
+Size : 11.1MB
+
+---
+
+Resources : Bat_Green
+StreamingAssets : Nothing
+InspectorLink : Bat_Green
+
+Size : 15.6MB
